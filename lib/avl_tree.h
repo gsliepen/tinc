90bf1b21fa7e94d73719da0593e7c0356d05e18f
Guus Sliepen <guus@tinc-vpn.org>
2001-01-06 18:21:17 +0000

    - Changed license of AVL tree library to GPL.

--- lib/avl_tree.h
+++ lib/avl_tree.h
@@ -5,19 +5,19 @@
                   2000 Guus Sliepen <guus@sliepen.warande.net>
                   2000 Wessel Dankers <wsl@nl.linux.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
     Original AVL tree library by Michael H. Buselli <cosine@cosine.org>.
 
@@ -29,7 +29,7 @@
     library for inclusion into tinc (http://tinc.nl.linux.org) by
     Guus Sliepen <guus@sliepen.warande.net>.
 
-    $Id: avl_tree.h,v 1.1.2.1 2001/01/05 23:50:56 guus Exp $
+    $Id: avl_tree.h,v 1.1.2.2 2001/01/06 18:21:17 guus Exp $
 */
 
 
