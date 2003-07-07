71f8124ea49f2a0e00e0cedbb1b76e49e9f1425d
Guus Sliepen <guus@tinc-vpn.org>
2003-07-07 11:50:52 +0000

    More missing definitions.

--- lib/ipv6.h
+++ lib/ipv6.h
@@ -17,7 +17,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-    $Id: ipv6.h,v 1.1.2.1 2003/07/07 11:13:31 guus Exp $
+    $Id: ipv6.h,v 1.1.2.2 2003/07/07 11:50:52 guus Exp $
 */
 
 #ifndef __TINC_IPV6_H__
@@ -74,6 +74,12 @@ struct ip6_hdr {
 	struct in6_addr ip6_src;
 	struct in6_addr ip6_dst;
 };
+#define ip6_vfc ip6_ctlun.ip6_un2_vfc
+#define ip6_flow ip6_ctlun.ip6_un1.ip6_un1_flow
+#define ip6_plen ip6_ctlun.ip6_un1.ip6_un1_plen
+#define ip6_nxt ip6_ctlun.ip6_un1.ip6_un1_nxt
+#define ip6_hlim ip6_ctlun.ip6_un1.ip6_un1_hlim
+#define ip6_hops ip6_ctlun.ip6_un1.ip6_un1_hlim
 
 struct icmp6_hdr {
 	uint8_t icmp6_type;
