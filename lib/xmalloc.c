b55813dc0b4a6a1f70c0f8d5f0512c8cebb4a5ba
Guus Sliepen <guus@tinc-vpn.org>
2006-12-16 16:34:04 +0000

    We do properly check for malloc and realloc.

--- lib/xmalloc.c
+++ lib/xmalloc.c
@@ -46,14 +46,6 @@ void *xcalloc (size_t n, size_t s);
 void *xrealloc (void *p, size_t n);
 #endif
 
-#ifndef HAVE_DONE_WORKING_MALLOC_CHECK
-#error you must run the autoconf test for a properly working malloc -- see malloc.m4
-#endif
-
-#ifndef HAVE_DONE_WORKING_REALLOC_CHECK
-#error you must run the autoconf test for a properly working realloc -- see realloc.m4
-#endif
-
 /* Exit value when the requested amount of memory is not available.
    The caller may set it to some other value.  */
 int xalloc_exit_failure = EXIT_FAILURE;
