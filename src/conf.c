e9576632dc4b780b867044269d06cc50f76d8c05
Guus Sliepen <guus@tinc-vpn.org>
2008-12-22 20:27:52 +0000

    Update copyright information.

--- src/conf.c
+++ src/conf.c
@@ -2,7 +2,7 @@
     conf.c -- configuration code
     Copyright (C) 1998 Robert van der Meulen
                   1998-2005 Ivo Timmermans
-                  2000-2006 Guus Sliepen <guus@tinc-vpn.org>
+                  2000-2008 Guus Sliepen <guus@tinc-vpn.org>
 		  2000 Cris van Pelt
 
     This program is free software; you can redistribute it and/or modify
