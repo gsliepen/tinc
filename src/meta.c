54c900e961de6065f607f5661edeb7c84be29ea5
Guus Sliepen <guus@tinc-vpn.org>
2011-05-22 14:02:27 +0200

    Reset tcplen after use.

--- src/meta.c
+++ src/meta.c
@@ -137,6 +137,7 @@ bool receive_meta(connection_t *c) {
 			if(c->tcplen) {
 				char *tcpbuffer = buffer_read(&c->inbuf, c->tcplen);
 				if(tcpbuffer) {
+					c->tcplen = 0;
 					receive_tcppacket(c, tcpbuffer, c->tcplen);
 					continue;
 				} else {
