gsliepen/tinc

commit dd07c9fc1f37bed8d1f67ffe7b203f61e7914edf

Check HMAC before sequence number.

src/sptps.c
@@ -439,6 +439,19 @@ static bool sptps_receive_data_datagram(sptps_t *s, const char *data, size_t len
 		return receive_handshake(s, data + 5, len - 5);
 	}
 
+	// Check HMAC.
+	uint16_t netlen = htons(len - 21);
+
+	char buffer[len + 23];
+
+	memcpy(buffer, &netlen, 2);
+	memcpy(buffer + 2, data, len);
+
+	memcpy(&seqno, buffer + 2, 4);
+
+	if(!digest_verify(&s->indigest, buffer, len - 14, buffer + len - 14))
+		return error(s, EIO, "Invalid HMAC");
+
 	// Replay protection using a sliding window of configurable size.
 	// s->inseqno is expected sequence number
 	// seqno is received sequence number
@@ -473,19 +486,8 @@ static bool sptps_receive_data_datagram(sptps_t *s, const char *data, size_t len
 	if(seqno > s->inseqno)
 		s->inseqno = seqno + 1;
 
-	uint16_t netlen = htons(len - 21);
 
-	char buffer[len + 23];
-
-	memcpy(buffer, &netlen, 2);
-	memcpy(buffer + 2, data, len);
-
-	memcpy(&seqno, buffer + 2, 4);
-
-	// Check HMAC and decrypt.
-	if(!digest_verify(&s->indigest, buffer, len - 14, buffer + len - 14))
-		return error(s, EIO, "Invalid HMAC");
-
+	// Decrypt.
 	cipher_set_counter(&s->incipher, &seqno, sizeof seqno);
 	if(!cipher_counter_xor(&s->incipher, buffer + 6, len - 4, buffer + 6))
 		return false;

commit 3a039ece25198c87e67950f0c4687587bf268075

Fix datagram SPTPS.
Commit dd07c9fc1f37bed8d1f67ffe7b203f61e7914edf broke the reception of datagram
SPTPS packets, by undoing the conversion of the sequence number to host byte
order before comparison. This caused error messages like "Packet is 16777215
seqs in the future, dropped (1)".

src/sptps.c
@@ -447,8 +447,6 @@ static bool sptps_receive_data_datagram(sptps_t *s, const char *data, size_t len
 	memcpy(buffer, &netlen, 2);
 	memcpy(buffer + 2, data, len);
 
-	memcpy(&seqno, buffer + 2, 4);
-
 	if(!digest_verify(&s->indigest, buffer, len - 14, buffer + len - 14))
 		return error(s, EIO, "Invalid HMAC");
 
@@ -492,6 +490,7 @@ static bool sptps_receive_data_datagram(sptps_t *s, const char *data, size_t len
 		s->received++;
 
 	// Decrypt.
+	memcpy(&seqno, buffer + 2, 4);
 	cipher_set_counter(&s->incipher, &seqno, sizeof seqno);
 	if(!cipher_counter_xor(&s->incipher, buffer + 6, len - 4, buffer + 6))
 		return false;