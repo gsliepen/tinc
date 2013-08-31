05a7f0b2fb07f8ee7752604a2a87b85f2430aaa3
Guus Sliepen <guus@tinc-vpn.org>
2013-09-01 00:39:14 +0200

    Start of a test suite.

---
+++ test/testlib.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Paths to executables
+
+tincd=../src/tincd
+tinc=../src/tinc
+sptps_test=../src/sptps_test
+
+# Test directories
+
+d1=$PWD/$_.1
+d2=$PWD/$_.2
+d3=$PWD/$_.3
+
+# Remove test directories
+
+rm -rf $d1 $d2 $d3
+
+# Default arguments for both tinc and tincd
+
+c1="--config=$d1 --pidfile=$d1/pid"
+c2="--config=$d2 --pidfile=$d2/pid"
+c3="--config=$d3 --pidfile=$d3/pid"
+
+# Arguments when running tincd
+
+r1="--logfile=$d1/log -d5"
+r2="--logfile=$d1/log -d5"
+r3="--logfile=$d1/log -d5"
+
+# Exit on errors, log all commands being executed
+
+set -ex
