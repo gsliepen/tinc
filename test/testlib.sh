4e7e4818b771af47a10ce0b8f4046ab455ef14a9
Guus Sliepen <guus@tinc-vpn.org>
2013-09-01 22:58:45 +0200

    Clean up leftover tincd and sptps_test processes.

--- test/testlib.sh
+++ test/testlib.sh
@@ -21,10 +21,6 @@ case "$_" in
 	;;
 esac
 
-# Remove test directories
-
-rm -rf $d1 $d2 $d3
-
 # Default arguments for both tinc and tincd
 
 c1="--config=$d1 --pidfile=$d1/pid"
@@ -36,6 +32,16 @@ c3="--config=$d3 --pidfile=$d3/pid"
 r1="--logfile=$d1/log -d5"
 r2="--logfile=$d2/log -d5"
 r3="--logfile=$d3/log -d5"
+
+# Check for leftover tinc daemons
+
+[ -f $d1/pid ] && $tinc $c1 stop
+[ -f $d2/pid ] && $tinc $c2 stop
+[ -f $d3/pid ] && $tinc $c3 stop
+
+# Remove test directories
+
+rm -rf $d1 $d2 $d3
 
 # Exit on errors, log all commands being executed
 
