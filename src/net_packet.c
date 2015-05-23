gsliepen/tinc

commit eb7a0db18ea71a44999d6a37b4b179dac0ed9bc7

Always keep UDP mappings alive for nodes that also have a meta-connection.
This is necessary for assisting with UDP hole punching. But we don't
need to know the PMTU for this, so only send UDP probes.

src/net_packet.c
@@ -1087,7 +1087,7 @@ static void try_mtu(node_t *n) {
    idle.
 */
 
-static void try_tx_sptps(node_t *n) {
+static void try_tx_sptps(node_t *n, bool mtu) {
 	/* If n is a TCP-only neighbor, we'll only use "cleartext" PACKET
 	   messages anyway, so there's no need for SPTPS at all. */
 
@@ -1110,13 +1110,14 @@ static void try_tx_sptps(node_t *n) {
 	/* If we do have a relay, try everything with that one instead. */
 
 	if(via != n)
-		return try_tx_sptps(via);
+		return try_tx_sptps(via, mtu);
 
 	try_udp(n);
-	try_mtu(n);
+	if(mtu)
+		try_mtu(n);
 }
 
-static void try_tx_legacy(node_t *n) {
+static void try_tx_legacy(node_t *n, bool mtu) {
 	/* Does he have our key? If not, send one. */
 
 	if(!n->status.validkey_in)
@@ -1133,7 +1134,15 @@ static void try_tx_legacy(node_t *n) {
 	}
 
 	try_udp(n);
-	try_mtu(n);
+	if(mtu)
+		try_mtu(n);
+}
+
+void try_tx(node_t *n, bool mtu) {
+	if(n->status.sptps)
+		try_tx_sptps(n, mtu);
+	else
+		try_tx_legacy(n, mtu);
 }
 
 void send_packet(node_t *n, vpn_packet_t *packet) {
@@ -1166,7 +1175,7 @@ void send_packet(node_t *n, vpn_packet_t *packet) {
 
 	if(n->status.sptps) {
 		send_sptps_packet(n, packet);
-		try_tx_sptps(n);
+		try_tx_sptps(n, true);
 		return;
 	}
 
@@ -1186,7 +1195,7 @@ void send_packet(node_t *n, vpn_packet_t *packet) {
 	}
 
 	send_udppacket(via, packet);
-	try_tx_legacy(via);
+	try_tx_legacy(via, true);
 }
 
 void broadcast_packet(const node_t *from, vpn_packet_t *packet) {

src/net.c
@@ -154,6 +154,7 @@ static void timeout_handler(void *data) {
 
 		if(c->last_ping_time + pingtimeout <= now.tv_sec) {
 			if(c->edge) {
+				try_tx(c->node, false);
 				if(c->status.pinged) {
 					logger(DEBUG_CONNECTIONS, LOG_INFO, "%s (%s) didn't respond to PING in %ld seconds", c->name, c->hostname, (long)now.tv_sec - c->last_ping_time);
 				} else if(c->last_ping_time + pinginterval <= now.tv_sec) {
@@ -170,6 +171,7 @@ static void timeout_handler(void *data) {
 			}
 			terminate_connection(c, c->edge);
 		}
+
 	}
 
 	timeout_set(data, &(struct timeval){pingtimeout, rand() % 100000});

src/net.h
@@ -214,6 +214,7 @@ extern void retry(void);
 extern int reload_configuration(void);
 extern void load_all_subnets(void);
 extern void load_all_nodes(void);
+extern void try_tx(struct node_t *n, bool);
 
 #ifndef HAVE_MINGW
 #define closesocket(s) close(s)

commit 8587e8c0d9ac997fcd2040470c1ccf5930bc18c3

Fix crashes when trying unreachable nodes.
timeout_handler() calls try_tx(c->node) when c->edge exists.
Unfortunately, the existence of c->edge is not enough to conclude that
the node is reachable.

In fact, during connection establishment, there is a short period of
time where we create an edge for the node at the other end of the
metaconnection, but we don't have one from the other side yet.
Unfortunately, if timeout_handler() runs during that short time
window, it will call try_tx() on an unreachable node, which makes
things explode because that function is not prepared to handle that
case.

A typical symptom of this race condition is a hard SEGFAULT while trying
to send packets using metaconnections that don't exist, due to
n->nexthop containing garbage.

This patch fixes the issue by making try_tx() check for reachability,
and then making all code paths use try_tx() instead of the more
specialized methods so that they go through the check.

This regression was introduced in
eb7a0db18ea71a44999d6a37b4b179dac0ed9bc7.

src/net_packet.c
@@ -1195,7 +1195,7 @@ static void try_tx_sptps(node_t *n, bool mtu) {
 	if(via != n) {
 		if((via->options >> 24) < 4)
 			return;
-		return try_tx_sptps(via, mtu);
+		return try_tx(via, mtu);
 	}
 
 	/* Otherwise, try to establish UDP connectivity. */
@@ -1208,7 +1208,7 @@ static void try_tx_sptps(node_t *n, bool mtu) {
 	   while we try to establish direct connectivity. */
 
 	if(!n->status.udp_confirmed && n != n->nexthop && (n->nexthop->options >> 24) >= 4)
-		try_tx_sptps(n->nexthop, mtu);
+		try_tx(n->nexthop, mtu);
 }
 
 static void try_tx_legacy(node_t *n, bool mtu) {
@@ -1233,6 +1233,8 @@ static void try_tx_legacy(node_t *n, bool mtu) {
 }
 
 void try_tx(node_t *n, bool mtu) {
+	if(!n->status.reachable)
+		return;
 	if(n->status.sptps)
 		try_tx_sptps(n, mtu);
 	else
@@ -1269,7 +1271,7 @@ void send_packet(node_t *n, vpn_packet_t *packet) {
 
 	if(n->status.sptps) {
 		send_sptps_packet(n, packet);
-		try_tx_sptps(n, true);
+		try_tx(n, true);
 		return;
 	}
 
@@ -1289,7 +1291,7 @@ void send_packet(node_t *n, vpn_packet_t *packet) {
 	}
 
 	send_udppacket(via, packet);
-	try_tx_legacy(via, true);
+	try_tx(via, true);
 }
 
 void broadcast_packet(const node_t *from, vpn_packet_t *packet) {
@@ -1472,7 +1474,7 @@ skip_harder:
 
 		if(to != myself) {
 			send_sptps_data(to, from, 0, DATA(&pkt), pkt.len);
-			try_tx_sptps(to, true);
+			try_tx(to, true);
 			return;
 		}
 	} else {