gsliepen/tinc

commit 6bc5d626a8726fc23365ee705761a3c666a08ad4

Drop libevent and use our own event handling again.
There are several reasons for this:

- MacOS/X doesn't support polling the tap device using kqueue, requiring a
  workaround to fall back to select().
- On Windows only sockets are properly handled, therefore tinc uses a second
  thread that does a blocking ReadFile() on the TAP-Win32/64 device. However,
  this does not mix well with libevent.
- Libevent, event just the core, is quite large, and although it is easy to get
  and install on many platforms, it can be a burden.
- Libev is more lightweight and seems technically superior, but it doesn't
  abstract away all the platform differences (for example, async events are not
  supported on Windows).

src/net_setup.c
@@ -42,7 +42,7 @@
 #include "xalloc.h"
 
 char *myport;
-static struct event device_ev;
+static io_t device_io;
 devops_t devops;
 
 char *proxyhost;
@@ -270,22 +270,16 @@ static bool read_rsa_private_key(void) {
 	return result;
 }
 
-static struct event keyexpire_event;
+static timeout_t keyexpire_timeout;
 
-static void keyexpire_handler(int fd, short events, void *data) {
+static void keyexpire_handler(void *data) {
 	regenerate_key();
+	timeout_set(data, &(struct timeval){keylifetime, rand() % 100000});
 }
 
 void regenerate_key(void) {
-	if(timeout_initialized(&keyexpire_event)) {
-		logger(DEBUG_STATUS, LOG_INFO, "Expiring symmetric keys");
-		event_del(&keyexpire_event);
-		send_key_changed();
-	} else {
-		timeout_set(&keyexpire_event, keyexpire_handler, NULL);
-	}
-
-	event_add(&keyexpire_event, &(struct timeval){keylifetime, rand() % 100000});
+	logger(DEBUG_STATUS, LOG_INFO, "Expiring symmetric keys");
+	send_key_changed();
 }
 
 /*
@@ -716,7 +710,8 @@ static bool setup_myself(void) {
 
 	free(cipher);
 
-	regenerate_key();
+	send_key_changed();
+	timeout_add(&keyexpire_timeout, keyexpire_handler, &keyexpire_timeout, &(struct timeval){keylifetime, rand() % 100000});
 
 	/* Check if we want to use message authentication codes... */
 
@@ -790,15 +785,8 @@ static bool setup_myself(void) {
 	if(!devops.setup())
 		return false;
 
-	if(device_fd >= 0) {
-		event_set(&device_ev, device_fd, EV_READ|EV_PERSIST, handle_device_data, NULL);
-
-		if (event_add(&device_ev, NULL) < 0) {
-			logger(DEBUG_ALWAYS, LOG_ERR, "event_add failed: %s", strerror(errno));
-			devops.close();
-			return false;
-		}
-	}
+	if(device_fd >= 0)
+		io_add(&device_io, handle_device_data, NULL, device_fd, IO_READ);
 
 	/* Run tinc-up script to further initialize the tap interface */
 	char *envp[5];
@@ -840,27 +828,16 @@ static bool setup_myself(void) {
 				return false;
 			}
 
-			listen_socket[i].tcp = i + 3;
-
 #ifdef FD_CLOEXEC
 			fcntl(i + 3, F_SETFD, FD_CLOEXEC);
 #endif
 
-			listen_socket[i].udp = setup_vpn_in_socket(&sa);
-			if(listen_socket[i].udp < 0)
+			int udp_fd = setup_vpn_in_socket(&sa);
+			if(udp_fd < 0)
 				return false;
 
-			event_set(&listen_socket[i].ev_tcp, listen_socket[i].tcp, EV_READ|EV_PERSIST, handle_new_meta_connection, NULL);
-			if(event_add(&listen_socket[i].ev_tcp, NULL) < 0) {
-				logger(DEBUG_ALWAYS, LOG_ERR, "event_add failed: %s", strerror(errno));
-				abort();
-			}
-
-			event_set(&listen_socket[i].ev_udp, listen_socket[i].udp, EV_READ|EV_PERSIST, handle_incoming_vpn_data, (void *)(intptr_t)listen_sockets);
-			if(event_add(&listen_socket[listen_sockets].ev_udp, NULL) < 0) {
-				logger(DEBUG_ALWAYS, LOG_ERR, "event_add failed: %s", strerror(errno));
-				abort();
-			}
+			io_add(&listen_socket[i].tcp, (io_cb_t)handle_new_meta_connection, &listen_socket[i], i + 3, IO_READ);
+			io_add(&listen_socket[i].udp, (io_cb_t)handle_incoming_vpn_data, &listen_socket[i], udp_fd, IO_READ);
 
 			if(debug_level >= DEBUG_CONNECTIONS) {
 				hostname = sockaddr2hostname(&sa);
@@ -913,37 +890,20 @@ static bool setup_myself(void) {
 					return false;
 				}
 
-				listen_socket[listen_sockets].tcp =
-					setup_listen_socket((sockaddr_t *) aip->ai_addr);
+				int tcp_fd = setup_listen_socket((sockaddr_t *) aip->ai_addr);
 
-				if(listen_socket[listen_sockets].tcp < 0)
+				if(tcp_fd < 0)
 					continue;
 
-				listen_socket[listen_sockets].udp =
-					setup_vpn_in_socket((sockaddr_t *) aip->ai_addr);
+				int udp_fd = setup_vpn_in_socket((sockaddr_t *) aip->ai_addr);
 
-				if(listen_socket[listen_sockets].udp < 0) {
-					close(listen_socket[listen_sockets].tcp);
+				if(tcp_fd < 0) {
+					close(tcp_fd);
 					continue;
 				}
 
-				event_set(&listen_socket[listen_sockets].ev_tcp,
-						  listen_socket[listen_sockets].tcp,
-						  EV_READ|EV_PERSIST,
-						  handle_new_meta_connection, NULL);
-				if(event_add(&listen_socket[listen_sockets].ev_tcp, NULL) < 0) {
-					logger(DEBUG_ALWAYS, LOG_ERR, "event_add failed: %s", strerror(errno));
-					abort();
-				}
-
-				event_set(&listen_socket[listen_sockets].ev_udp,
-						  listen_socket[listen_sockets].udp,
-						  EV_READ|EV_PERSIST,
-						  handle_incoming_vpn_data, (void *)(intptr_t)listen_sockets);
-				if(event_add(&listen_socket[listen_sockets].ev_udp, NULL) < 0) {
-					logger(DEBUG_ALWAYS, LOG_ERR, "event_add failed: %s", strerror(errno));
-					abort();
-				}
+				io_add(&listen_socket[listen_sockets].tcp, handle_new_meta_connection, &listen_socket[listen_sockets], tcp_fd, IO_READ);
+				io_add(&listen_socket[listen_sockets].udp, handle_incoming_vpn_data, &listen_socket[listen_sockets], udp_fd, IO_READ);
 
 				if(debug_level >= DEBUG_CONNECTIONS) {
 					hostname = sockaddr2hostname((sockaddr_t *) aip->ai_addr);
@@ -1025,10 +985,10 @@ void close_network_connections(void) {
 	}
 
 	for(int i = 0; i < listen_sockets; i++) {
-		event_del(&listen_socket[i].ev_tcp);
-		event_del(&listen_socket[i].ev_udp);
-		close(listen_socket[i].tcp);
-		close(listen_socket[i].udp);
+		io_del(&listen_socket[i].tcp);
+		io_del(&listen_socket[i].udp);
+		close(listen_socket[i].tcp.fd);
+		close(listen_socket[i].udp.fd);
 	}
 
 	char *envp[5];

README
@@ -50,7 +50,6 @@ In order to compile tinc, you will need a GNU C compiler environment. Please
 ensure you have the latest stable versions of all the required libraries:
 
 - OpenSSL (http://www.openssl.org/) version 1.0.0 or later.
-- Libevent (http://monkey.org/~provos/libevent/)
 
 The following libraries are used by default, but can be disabled if necessary:
 

README.git
@@ -8,7 +8,6 @@ to install the very latest versions of the following packages:
 - automake
 - autoconf
 - gettext
-- libevent
 
 Then you have to let the autotools create all the autogenerated files, using
 this command:

configure.in
@@ -183,7 +183,6 @@ AC_ARG_WITH(libgcrypt, AC_HELP_STRING([--with-libgcrypt], [enable use of libgcry
 
 tinc_CURSES
 tinc_READLINE
-tinc_LIBEVENT
 tinc_ZLIB
 tinc_LZO
 

doc/tinc.texi
@@ -339,7 +339,6 @@ having them installed, configure will give you an error message, and stop.
 * OpenSSL::
 * zlib::
 * lzo::
-* libevent::
 * libcurses::
 * libreadline::
 @end menu
@@ -453,27 +452,6 @@ of this package.
 
 If you have to install lzo manually, you can get the source code
 from @url{http://www.oberhumer.com/opensource/lzo/}.  Instructions on how to configure,
-build and install this package are included within the package.  Please
-make sure you build development and runtime libraries (which is the
-default).
-
-
-@c ==================================================================
-@node       libevent
-@subsection libevent
-
-@cindex libevent
-For the main event loop, tinc uses the libevent library.
-
-If this library is not installed, you wil get an error when configuring
-tinc for build.
-
-You can use your operating system's package manager to install this if
-available.  Make sure you install the development AND runtime versions
-of this package.
-
-If you have to install libevent manually, you can get the source code
-from @url{http://libevent.org/}. Instructions on how to configure,
 build and install this package are included within the package.  Please
 make sure you build development and runtime libraries (which is the
 default).

have.h
@@ -196,10 +196,6 @@
 #include <netinet/if_ether.h>
 #endif
 
-#ifdef HAVE_EVENT_H
-#include <event.h>
-#endif
-
 #ifdef HAVE_MINGW
 #define SLASH "\\"
 #else

m4/libevent.m4
@@ -1,33 +1,0 @@
-dnl Check to find the libevent headers/libraries
-
-AC_DEFUN([tinc_LIBEVENT],
-[
-  AC_ARG_WITH(libevent,
-    AS_HELP_STRING([--with-libevent=DIR], [libevent base directory, or:]),
-    [libevent="$withval"
-     CPPFLAGS="$CPPFLAGS -I$withval/include"
-     LDFLAGS="$LDFLAGS -L$withval/lib"]
-  )
-
-  AC_ARG_WITH(libevent-include,
-    AS_HELP_STRING([--with-libevent-include=DIR], [libevent headers directory]),
-    [libevent_include="$withval"
-     CPPFLAGS="$CPPFLAGS -I$withval"]
-  )
-
-  AC_ARG_WITH(libevent-lib,
-    AS_HELP_STRING([--with-libevent-lib=DIR], [libevent library directory]),
-    [libevent_lib="$withval"
-     LDFLAGS="$LDFLAGS -L$withval"]
-  )
-
-  AC_CHECK_HEADERS(event.h,
-    [],
-    [AC_MSG_ERROR("libevent header files not found."); break]
-  )
-
-  AC_CHECK_LIB(event, event_init,
-    [LIBS="-levent $LIBS"],
-    [AC_MSG_ERROR("libevent libraries not found.")]
-  )
-])

src/Makefile.am
@@ -8,7 +8,7 @@ tincd_SOURCES = \
 	utils.c getopt.c getopt1.c list.c splay_tree.c dropin.c fake-getaddrinfo.c fake-getnameinfo.c hash.c \
 	buffer.c conf.c connection.c control.c edge.c graph.c logger.c meta.c net.c net_packet.c net_setup.c \
 	net_socket.c netutl.c node.c process.c protocol.c protocol_auth.c protocol_edge.c protocol_misc.c \
-	protocol_key.c protocol_subnet.c route.c sptps.c subnet.c subnet_parse.c tincd.c \
+	protocol_key.c protocol_subnet.c route.c sptps.c subnet.c subnet_parse.c event.c tincd.c \
 	dummy_device.c raw_socket_device.c multicast_device.c
 	
 if UML
@@ -46,7 +46,7 @@ INCLUDES = @INCLUDES@ -I$(top_builddir)
 noinst_HEADERS = \
 	xalloc.h utils.h getopt.h list.h splay_tree.h dropin.h fake-getaddrinfo.h fake-getnameinfo.h fake-gai-errnos.h ipv6.h ipv4.h ethernet.h \
 	buffer.h conf.h connection.h control.h control_common.h device.h edge.h graph.h info.h logger.h meta.h net.h netutl.h node.h process.h \
-	protocol.h route.h subnet.h sptps.h tincctl.h top.h bsd/tunemu.h hash.h
+	protocol.h route.h subnet.h sptps.h tincctl.h top.h bsd/tunemu.h hash.h event.h
 
 nodist_noinst_HEADERS = \
 	cipher.h crypto.h ecdh.h ecdsa.h digest.h prf.h rsa.h ecdsagen.h rsagen.h

commit 2784a171ec39e2a34aabf8194a651de570e19e0e

Fix error handling when setting up the UDP socket.
Due to this typo, if tinc managed to set up the TCP socket but not the
UDP socket, it would continue anyway.

The regression was introduced in
6bc5d626a8726fc23365ee705761a3c666a08ad4.

src/net_setup.c
@@ -676,7 +676,7 @@ static bool add_listen_address(char *address, bool bindto) {
 
 		int udp_fd = setup_vpn_in_socket((sockaddr_t *) aip->ai_addr);
 
-		if(tcp_fd < 0) {
+		if(udp_fd < 0) {
 			close(tcp_fd);
 			continue;
 		}