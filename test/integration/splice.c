94520716cb6ca59fc83c0c39021a085b42cda54b
Kirill Isakov <bootctl@gmail.com>
2022-03-23 12:49:09 +0600

    Move integration tests into a subdirectory

--- test/integration/splice.c
+++ test/integration/splice.c
@@ -17,7 +17,7 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-#include "../src/system.h"
+#include "../../src/system.h"
 
 #ifdef HAVE_MINGW
 static const char *winerror(int err) {
